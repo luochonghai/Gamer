d1acd7b3ad679c7e16ec4b624f4fcc114c49bb1f
GeorgeRobots <1258501438@qq.com>
2018-06-12 22:56:46 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SE.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	Py_Initialize();
+	QApplication a(argc, argv);
+	SE w;
+	w.show();
+	return a.exec();
+}
